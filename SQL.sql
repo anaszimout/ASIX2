ed85503ef864751526be750b93c8fa5459abbd30
anaszimout <94825968+anaszimout@users.noreply.github.com>
2022-11-17 20:47:32 +0100

    Delete SQL.sql

--- SQL.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE mitabla ( id INT PRIMARY KEY, nombre VARCHAR(20) );
-INSERT INTO mitabla VALUES ( 1, 'Will' );
-INSERT INTO mitabla VALUES ( 2, 'Marry' );
-INSERT INTO mitabla VALUES ( 3, 'Dean' );
-SELECT id, nombre FROM mitabla WHERE id = 1;
-UPDATE mitabla SET nombre = 'Willy' WHERE id = 1;
-SELECT id, nombre FROM mitabla;
-DELETE FROM mitabla WHERE id = 1;
-SELECT id, nombre FROM mitabla;
